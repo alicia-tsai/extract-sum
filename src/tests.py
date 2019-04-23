836b62b962697690b3f5609f05d9f92e8d73963e
alicia-tsai <alicia.tsai@gatech.edu>
2019-04-22 18:02:44 -0700

    add get tfidf test

--- src/tests.py
+++ src/tests.py
@@ -1,14 +1,27 @@
 import unittest
+
+import numpy as np
+import scipy
+
 import data
 import utils
 
 class TestExtractSum(unittest.TestCase):
 
+    def setUp(self):
+        """Set up data for the project."""
+        self.titles, self.refs, self.docs = data.get_outlook_data()
+
     def test_get_outlook_data(self):
-        title, ref, text = data.get_outlook_data()
-        num_title, num_ref, num_text = len(title), len(ref), len(text)
-        self.assertEqual(True, num_title > 0)
-        self.assertEqual(True, num_title == num_ref == num_text)
+        num_titles, num_refs, num_docs = len(self.titles), len(self.refs), len(self.docs)
+        self.assertTrue(num_titles > 0)
+        self.assertTrue(num_titles == num_refs == num_docs)
+
+    def test_get_tfidf_matrix(self):
+        doc = self.docs[0]
+        X = utils.vectorize_text(doc)
+        self.assertEqual(scipy.sparse.csr.csr_matrix, type(X))
+        self.assertTrue(scipy.sparse.linalg.norm(X) > 0)
 
 
 if __name__ == '__main__':
