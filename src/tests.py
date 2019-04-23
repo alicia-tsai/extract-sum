9fa810de56665266541a3cd77bbec5337df45115
Alicia Tsai <aliciatsai@berkeley.edu>
2019-04-23 00:38:30 -0700

    add get data test

--- src/tests.py
+++ src/tests.py
@@ -14,6 +14,11 @@ class TestExtractSum(unittest.TestCase):
         self.titles, self.refs, self.docs = data.get_outlook_data()
         self.doc = self.docs[0]
 
+    def test_get_outlook_data(self):
+        num_titles, num_refs, num_docs = len(self.titles), len(self.refs), len(self.docs)
+        self.assertTrue(num_titles > 0)
+        self.assertTrue(num_titles == num_refs == num_docs)
+
 
 if __name__ == '__main__':
     unittest.main()
