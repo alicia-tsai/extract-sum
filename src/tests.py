c2de8d66d63df4c95cb095523e6e2787aeebfc86
Alicia Tsai <aliciatsai@berkeley.edu>
2019-04-23 00:10:24 -0700

    add fourth test

--- src/tests.py
+++ src/tests.py
@@ -11,6 +11,7 @@ class TestExtractSum(unittest.TestCase):
     def setUp(self):
         """Set up data for the project."""
         self.titles, self.refs, self.docs = data.get_outlook_data()
+        self.doc = self.docs[0]
 
     def test_get_outlook_data(self):
         num_titles, num_refs, num_docs = len(self.titles), len(self.refs), len(self.docs)
@@ -18,20 +19,24 @@ class TestExtractSum(unittest.TestCase):
         self.assertTrue(num_titles == num_refs == num_docs)
 
     def test_split_sentence(self):
-        doc = self.docs[0]
-        sentences = utils.split_sentence(doc)
-
-        self.assertEqual(str, type(doc))
+        sentences = utils.split_sentence(self.doc)
+        self.assertEqual(str, type(self.doc))
         self.assertTrue(len(sentences) > 0)
         self.assertEqual(str, type(sentences[0]))
 
     def test_get_tfidf_matrix(self):
-        doc = self.docs[0]
-        X = utils.vectorize_text(doc)
+        X = utils.vectorize_text(self.doc)
         self.assertEqual(scipy.sparse.csr.csr_matrix, type(X))
         self.assertTrue(scipy.sparse.linalg.norm(X) > 0)
         self.assertTrue(X.shape[0] > 0)
         self.assertTrue(X.shape[1] > 1)
+
+    def test_get_summary(self):
+        k = 5
+        X = utils.vectorize_text(self.doc)
+        random_exemplar_indices = [np.random.randint(X.shape[0]) for _ in range(k)]
+        summary = utils.get_summary(self.doc, random_exemplar_indices, verbose=False)
+        self.assertEqual(str, type(summary))
 
 
 if __name__ == '__main__':
