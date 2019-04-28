9f2e2393e75ecdda9c346e44c135c6cef9a4106a
Alicia Tsai <aliciatsai@berkeley.edu>
2019-04-28 15:15:32 -0700

    Update tests.py

--- src/tests.py
+++ src/tests.py
@@ -71,24 +71,6 @@ class TestExtractSum(unittest.TestCase):
             for subkey in scores[key].keys():
                 self.assertTrue(scores[key][subkey] >= 0)
 
-    def test_get_word_embedding_rouge_score(self):
-        X = utils.vectorize_text(self.doc)
-        random_exemplar_indices = [np.random.randint(X.shape[0]) for _ in range(5)]
-        summary = utils.get_summary(self.doc, random_exemplar_indices, verbose=False)
-        scores = utils.get_rouge_score(summary, self.refs[0], verbose=False, rouge_embed=True, embed_dict='en_core_web_sm')
-        self.assertEqual(dict, type(scores))
-        self.assertTrue(len(scores.keys()) == 3)
-        for key in scores.keys():
-            self.assertEqual(dict, type(scores[key]))
-            self.assertTrue(len(scores[key].keys()) == 3)
-            for subkey in scores[key].keys():
-                self.assertTrue(scores[key][subkey] >= 0)
-
-    def test_start_matlab_engine(self):
-        import matlab
-        eng = utils.start_matlab_engine()
-        self.assertEqual(matlab.engine.matlabengine.MatlabEngine, type(eng))
-
 
 if __name__ == '__main__':
     unittest.main()
