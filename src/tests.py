7424eb041bef762496491c630820ac46bd48fd6c
Alicia Tsai <aliciatsai@berkeley.edu>
2019-04-28 16:14:46 -0700

    add get news data test

--- src/tests.py
+++ src/tests.py
@@ -14,6 +14,11 @@ class TestExtractSum(unittest.TestCase):
         self.titles, self.refs, self.docs = data.get_newsroom_data()
         self.doc = self.docs[0]
 
+    def test_get_news_data(self):
+        num_titles, num_refs, num_docs = len(self.titles), len(self.refs), len(self.docs)
+        self.assertTrue(num_titles > 0)
+        self.assertTrue(num_titles == num_refs == num_docs)
+
 
 
 if __name__ == '__main__':
